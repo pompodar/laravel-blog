637226ab93f78bdfbd44b7daf5b2dd304d074b3e
pompodar <pompodar@gmail.com>
2023-10-17 09:48:55 +0300

    move postform

--- resources/js/components/Posts.jsx
+++ resources/js/components/Posts.jsx
@@ -1,60 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-
 import Comment from './Comment';
-
-function PostForm({ onPostCreated }) {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-        const requestBody = {
-            title: title,
-            content: content,
-        };
-
-        console.log(requestBody);
-
-        fetch(`/posts/new-post`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': csrfToken,
-            },
-            body: JSON.stringify(requestBody),
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then((newPost) => {
-                onPostCreated(newPost); // Notify the parent component that a new post was created
-                setTitle('');
-                setContent('');
-            })
-            .catch((error) => console.error('Error creating a new post:', error));
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Title:</label>
-                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-            </div>
-            <div>
-                <label>Content:</label>
-                <textarea value={content} onChange={(e) => setContent(e.target.value)} />
-            </div>
-            <button type="submit">Create Post</button>
-        </form>
-    );
-}
+import PostForm from './PostForm';
 
 function Posts() {
     const [data, setData] = useState([]);
